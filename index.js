149c16f06f75d9b853b3da104c046ecf83e6e872
yugmani <yugmani@yahoo.com>
2022-04-06 23:04:21 -0700

    objects and prototypes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Import stylesheets
 import './style.css';
 
+// JavaScript classes
+// **************************************
+
 class Character {
   constructor(name, health, strength) {
     this.name = name;
@@ -30,4 +33,27 @@ aurora.xp += 15;
 console.log(aurora.describe()); //Aurora has 130 health points, 35 as strength and 15 XP points
 console.log(glacius.describe()); //Glacius has 130 health points, 30 as strength and 0 XP points
 
+//Under the hood: objects and prototypes
+/* ******************************************* */
 
+const anObject = {
+  myProp: 2,
+};
+
+//Create anotherObject using anObject as a prototype
+const anotherObject = Object.create(anObject);
+
+console.log('Another Object: ', anotherObject.myProp); //Another Object: 2
+// In this example, the JavaScript statement Object.create() is used to create the object anotherObject with object anObject as its prototype.
+
+anotherObject.myProp = 102;
+console.log(anotherObject.myProp); //102
+
+// Create yetAnotherObject using anotherObject as a prototype
+const yetAnotherObject = Object.create(anotherObject);
+
+//// myProp is found in yetAnotherObject's prototype chain (in anObject)
+console.log('yetAnotherObject: ', yetAnotherObject.myProp); //yetAnotherObject: 102
+
+// myOtherProp can't be found in yetAnotherObject's prototype chain
+console.log(yetAnotherObject.myOtherProp); //undefined
