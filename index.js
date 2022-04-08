77f152e0455e0c065471bb4605f28d65dba414ae
yugmani <yugmani@yahoo.com>
2022-04-07 22:41:26 -0700

    dog class

--- index.js
+++ index.js
@@ -96,13 +96,13 @@ console.log(yetAnotherObject.myOtherProp); //undefined
 
 console.log('Welcome to the adventure! Here are our heroes:');
 console.log(aurora.describe()); //Aurora has 130 health points, 35 as strength and 15 XP points
-console.log(glacius.describe());  //Glacius has 130 health points, 30 as strength and 0 XP points
+console.log(glacius.describe()); //Glacius has 130 health points, 30 as strength and 0 XP points
 console.log('********************************');
 
 const monster = new Character('Spike', 40, 20);
 console.log("A wild monster was appeared: it's named " + monster.name);
 
-monster.attack(aurora); 
+monster.attack(aurora);
 // Spike attacks Aurora and causes 20 damage points
 //Aurora has 110 health points left
 monster.attack(glacius);
@@ -124,3 +124,42 @@ console.log('********************************');
 console.log(glacius.describe());
 // Glacius has 110 health points, 30 as strength and 10 XP points
 console.log('********************************');
+
+// Coding time!
+// ***************************************
+
+// Dogs
+// -----------------------------------------
+// Complete the following program to add the definition of the Dog class.
+
+// Dogs taller than 60 emote "Grrr! Grrr!" when they bark, other ones yip "Woof! Woof!".
+
+// TODO: define the Dog class here
+class Dog {
+  constructor(name, species, size) {
+    this.name = name;
+    this.species = species;
+    this.size = size;
+  }
+
+  bark() {
+    if (this.size > 60) {
+      return 'Grrr! Grrr!';
+    } else {
+      return 'Woof! Woof!';
+    }
+  }
+}
+
+const fang = new Dog('Fang', 'boarhound', 75);
+console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`); //Fang is a boarhound dog measuring 75
+console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+// Look, a cat! Fang barks: Grrr! Grrr!
+
+console.log('********************************');
+
+const snowy = new Dog('Snowy', 'terrier', 22);
+console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
+//Snowy is a terrier dog measuring 22
+console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
+// Look, a cat! Snowy barks: Woof! Woof!
