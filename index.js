6e507483515cd07694c515a091e83ac6cc4a20a8
yugmani <yugmani@yahoo.com>
2022-04-07 22:22:04 -0700

    RPG attack

--- index.js
+++ index.js
@@ -16,6 +16,31 @@ class Character {
   describe() {
     return `${this.name} has ${this.health} health points, ${this.strength} as strength and ${this.xp} XP points`;
   }
+
+  // Attack a target
+  attack(target) {
+    if (this.health > 0) {
+      const damage = this.strength;
+      console.log(
+        `${this.name} attacks ${target.name} and causes ${damage} damage points`
+      );
+      target.health -= damage;
+      if (target.health > 0) {
+        console.log(`${target.name} has ${target.health} health points left`);
+      } else {
+        target.health = 0;
+        const bonusXP = 10;
+        console.log(
+          `${this.name} eliminated ${target.name} and wins ${bonusXP} experience points`
+        );
+        this.xp += bonusXP;
+      }
+    } else {
+      console.log(`{this.name} can't attack (they've been eliminated)`);
+    }
+    //Return the character description
+    return `${this.name} has ${this.health} health points, ${this.strength} as strength and ${this.xp} XP points`;
+  }
 }
 
 const aurora = new Character('Aurora', 150, 25);
@@ -57,3 +82,45 @@ console.log('yetAnotherObject: ', yetAnotherObject.myProp); //yetAnotherObject: 
 
 // myOtherProp can't be found in yetAnotherObject's prototype chain
 console.log(yetAnotherObject.myOtherProp); //undefined
+
+//Monsters and fights, of course!
+// ****************************************
+
+// Following is how a fight will be handled. If attacked, a character sees their life points decrease from the strength of the attacker. If its health value falls below zero, the character is considered dead and cannot attack anymore. Its vanquisher receives a fixed number of 10 experience points.
+
+//First, let's add the capability for our characters to fight one another. Since it's a shared ability, we define it as a method named attack() in the Character class.
+
+//Attack a target;
+
+//Now we can introduce a monster in the game and make it fight our players. Here's the rest of the final code of our RPG.
+
+console.log('Welcome to the adventure! Here are our heroes:');
+console.log(aurora.describe()); //Aurora has 130 health points, 35 as strength and 15 XP points
+console.log(glacius.describe());  //Glacius has 130 health points, 30 as strength and 0 XP points
+console.log('********************************');
+
+const monster = new Character('Spike', 40, 20);
+console.log("A wild monster was appeared: it's named " + monster.name);
+
+monster.attack(aurora); 
+// Spike attacks Aurora and causes 20 damage points
+//Aurora has 110 health points left
+monster.attack(glacius);
+// Spike attacks Glacius and causes 20 damage points
+// Glacius has 110 health points left
+
+console.log('********************************');
+aurora.attack(monster);
+// Aurora attacks Spike and causes 35 damage points
+// Spike has 5 health points left
+glacius.attack(monster);
+// Glacius attacks Spike and causes 30 damage points
+// Glacius eliminated Spike and wins 10 experience points
+console.log('********************************');
+
+console.log(aurora.describe());
+// Aurora has 110 health points, 35 as strength and 15 XP points
+console.log('********************************');
+console.log(glacius.describe());
+// Glacius has 110 health points, 30 as strength and 10 XP points
+console.log('********************************');
